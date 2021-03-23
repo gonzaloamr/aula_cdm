782898983ee6c8d7cbfe1c6e83dfe3a4f9925818
Gonzalo Rodriguez <gonzalo.amr@gmail.com>
2021-03-23 02:43:15 -0300

    Terminado as navegações entre activities

--- app/src/main/java/com/example/primeiroteste/PaginaTresActivity.java
+++ app/src/main/java/com/example/primeiroteste/PaginaTresActivity.java
@@ -6,19 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class PaginaUmActivity extends AppCompatActivity {
+public class PaginaTresActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pagina_um);
+        setContentView(R.layout.activity_pagina_tres);
     }
-    public void abreTelaDois (View view){
+
+    public void voltaTelaDois (View view){
         Intent intent = new Intent(this,TelaDoisActivity.class);
         startActivity(intent);
 
     }
 }
-
-
-
--- app/src/main/java/com/example/primeiroteste/TelaDoisActivity.java
+++ app/src/main/java/com/example/primeiroteste/TelaDoisActivity.java
@@ -2,7 +2,9 @@ package com.example.primeiroteste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TelaDoisActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class TelaDoisActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_dois);
+    }
+
+    public void voltaPaginaUm (View view){
+        Intent intent = new Intent(this,PaginaUmActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void abrePaginaTres (View view){
+        Intent intent = new Intent(this,PaginaTresActivity.class);
+        startActivity(intent);
+
     }
 }
